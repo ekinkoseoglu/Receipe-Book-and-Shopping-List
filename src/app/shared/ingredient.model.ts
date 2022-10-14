54e423629a04cf5183bab8e52af3faa8c6e9f40f
Ekin Koseoglu <ekindenizkoseoglu@gmail.com>
2022-10-14 20:11:31 +0300

    Created Baic forms of Shopping-list and Recipes Component

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,18 @@
+export class Ingredient{
+    constructor(public name:string, public amount:number){}
+}
+
+
+
+// Same as above but with a shortcut
+// --------------------------------------------------------------------
+
+    // export class Ingredient{
+    //     public name:string;
+    //     public amount:number;
+    //     constructor(name:string,amount:number){
+    //         this.name=name;
+    //         this.amount=amount;
+    //     }
+    
+    // }
