f9b344701e6bb44b629e167c31591b5df53d8106
Ekin Koseoglu <ekindenizkoseoglu@gmail.com>
2022-10-22 16:41:51 +0300

    Added Custom Attribute Directive

---
+++ src/app/shared/directives/appHighlight.directive.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { AppHighlightDirective } from './appHighlight.directive';
+
+describe('Directive: AppHighlight', () => {
+  it('should create an instance', () => {
+    const directive = new AppHighlightDirective();
+    expect(directive).toBeTruthy();
+  });
+});
